b440717241c58fb7b46517d79cad47200046b364
Lorenzo Gimenez Luzny <lgimenez@fi.uba.ar>
2024-04-09 21:25:15 -0300

    reorganize domain problems

--- test/api/evaluate/test_domain.py
+++ test/api/evaluate/test_domain.py
@@ -5,50 +5,35 @@ from rest_framework.test import APITestCase
 
 class APITests(APITestCase):
 
-    def test_evaluate_domain_zeroed_root(self):
-        data = {
-            'problem_input': {'expression': '1/x', 'variables': []},
-            'type': 'domain'
-        }
-
-        response = self.client.post(path='/validations/evaluate', data=data, format='json')
-
-        body = json.loads(response.content)
-        self.assertEquals(response.status_code, status.HTTP_200_OK)
-        self.assertEquals(body['result']['expression'], '\\left(-\\infty, 0\\right) \\cup \\left(0, \\infty\\right)')
-
-    def test_evaluate_domain_real_root(self):
-        data = {
-            'problem_input': {'expression': '1/(x-2)', 'variables': []},
-            'type': 'domain'
-        }
-
-        response = self.client.post(path='/validations/evaluate', data=data, format='json')
-
-        body = json.loads(response.content)
-        self.assertEquals(response.status_code, status.HTTP_200_OK)
-        self.assertEquals(body['result']['expression'], '\\left(-\\infty, 2\\right) \\cup \\left(2, \\infty\\right)')
-
-    def test_evaluate_domain_double_zeroed_root(self):
-        data = {
-            'problem_input': {'expression': '1/(x^2)', 'variables': []},
-            'type': 'domain'
-        }
-
-        response = self.client.post(path='/validations/evaluate', data=data, format='json')
-
-        body = json.loads(response.content)
-        self.assertEquals(response.status_code, status.HTTP_200_OK)
-        self.assertEquals(body['result']['expression'], '\\left(-\\infty, 0\\right) \\cup \\left(0, \\infty\\right)')
-
-    def test_evaluate_domain_no_root(self):
-        data = {
+    rational_domains = [
+        {
             'problem_input': {'expression': 'x', 'variables': []},
-            'type': 'domain'
+            'problem_output': '\mathbb{R}'
+        },
+        {
+            'problem_input': {'expression': '1/x', 'variables': []},
+            'problem_output': '\\left(-\\infty, 0\\right) \\cup \\left(0, \\infty\\right)'
+        },
+        {
+            'problem_input': {'expression': '1/(x-2)', 'variables': []},
+            'problem_output': '\\left(-\\infty, 2\\right) \\cup \\left(2, \\infty\\right)'
+        },
+        {
+            'problem_input': {'expression': '1/(x^2)', 'variables': []},
+            'problem_output': '\\left(-\\infty, 0\\right) \\cup \\left(0, \\infty\\right)'
         }
+    ]
 
-        response = self.client.post(path='/validations/evaluate', data=data, format='json')
+    def test_evaluate_rational_domains(self):
+        for exercise in self.rational_domains:
+            data = {
+                'problem_input': exercise['problem_input'],
+                'type': 'domain'
+            }
 
-        body = json.loads(response.content)
-        self.assertEquals(response.status_code, status.HTTP_200_OK)
-        self.assertEquals(body['result']['expression'], '\mathbb{R}')
+            response = self.client.post(path='/validations/evaluate', data=data, format='json')
+
+            body = json.loads(response.content)
+            self.assertEquals(response.status_code, status.HTTP_200_OK)
+            self.assertEquals(body['result']['expression'], exercise['problem_output'])
+
